b5fd49021846978da77e9e3b9769ec12daab6b5b
Davey Holler <daveyholler@hey.com>
2021-12-21 11:20:59 -0800

    version 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Version Two</h1>
+      <h1>This is Version Two</h1>
       <iframe src="https://giphy.com/embed/af0sYEelThfNu" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+      <p>It has a paragraph tag</p>
     </div>
   );
 }
