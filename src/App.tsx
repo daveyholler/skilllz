d1523627e53b7a0cd561bbe3465b6d4bcd9b3e7d
Davey Holler <daveyholler@hey.com>
2021-12-21 11:11:40 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Version One</h1>
+      <iframe src="https://giphy.com/embed/WJjLyXCVvro2I" width="480" height="423" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
     </div>
   );
 }
