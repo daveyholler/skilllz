7d429ba186db8fc364be30d3007d8ef6ec415573
Davey Holler <daveyholler@hey.com>
2021-12-21 14:46:12 -0800

    version 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is Version Two</h1>
-      <iframe src="https://giphy.com/embed/af0sYEelThfNu" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
-      <p>It has a paragraph tag</p>
+      <h1>Version Three</h1>
+      <iframe src="https://giphy.com/embed/l1J9I5KFN8zwGG4k8" width="480" height="369" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+      <p>It <strong>does not</strong> have a paragraph tag</p>
     </div>
   );
 }
